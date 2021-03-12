40c9f0b5e158b523a9828d883cce6e519767c8fc
Dan Witte <dan@Dans-MacBook-Air-913.local>
2021-03-12 11:02:36 -0500

    adding c_dim

--- models/decoders/siren_mlp.py
+++ models/decoders/siren_mlp.py
@@ -1,21 +1,20 @@
 import torch
 import numpy as np
 import torch.nn as nn
-
+import pdb
 
 # https://github.com/vsitzmann/siren/blob/4df34baee3f0f9c8f351630992c1fe1f69114b5f/modules.py#L622
 def sine_init(m):
     with torch.no_grad():
-        if hasattr(m, 'weight'):
+        if hasattr(m, "weight"):
             num_input = m.weight.size(-1)
             # See supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(
-                -np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
+            m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
 
 
 def first_layer_sine_init(m):
     with torch.no_grad():
-        if hasattr(m, 'weight'):
+        if hasattr(m, "weight"):
             num_input = m.weight.size(-1)
             # See paper sec. 3.2, final paragraph, and supplement
             # Sec. 1.5 for discussion of factor 30
@@ -42,11 +41,12 @@ class Decoder(nn.Module):
         sigma_condition: True
         xyz_condition: True
     """
+
     def __init__(self, _, cfg):
         super().__init__()
         self.cfg = cfg
         self.z_dim = cfg.z_dim
-        self.dim = dim = cfg.dim
+        self.dim = dim = cfg.dim + self.z_dim + 1
         self.out_dim = out_dim = cfg.out_dim
         self.hidden_size = hidden_size = cfg.hidden_size
         self.n_blocks = n_blocks = cfg.n_blocks
@@ -64,15 +64,18 @@ class Decoder(nn.Module):
         self.blocks[0].apply(first_layer_sine_init)
 
     # This should have the same signature as the sig condition one
-    def forward(self, x, c):
+    def forward(self, x, c_dims):
         """
         :param x: (bs, npoints, self.dim) Input coordinate (xyz)
         :param c: (bs, self.zdim + 1) Shape latent code + sigma
         TODO: will ignore [c] for now
         :return: (bs, npoints, self.dim) Gradient (self.dim dimension)
         """
-        # net = x.transpose(1, 2)  # (bs, dim, n_points)
-        net = x  # (bs, n_points, dim)
+        p_dims = x.transpose(1, 2)  # (bs, dim, n_points)
+        batch_size, D, num_points = p_dims.size()
+        c_expand = c_dims.unsqueeze(2).expand(-1, -1, num_points)
+        c_xyz = torch.cat([p_dims, c_expand], dim=1).view(batch_size, num_points, -1)
+        net = c_xyz
         for block in self.blocks[:-1]:
             net = self.act(block(net))
         out = self.blocks[-1](net)
